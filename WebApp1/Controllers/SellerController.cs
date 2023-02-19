1766ed426051fba5381749743c65480d9e67a9a1
Prakash Kumar <101341685@georgebrown.ca>
2023-02-19 03:19:09 -0500

    SellerPanel Structure

---
+++ WebApp1/Controllers/SellerController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp1.Controllers
+{
+    public class SellerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AddItem()
+        {
+            return View();
+        }
+    }
+}
