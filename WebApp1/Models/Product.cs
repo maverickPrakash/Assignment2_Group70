a83f4b69c65121c43d3ac10edd645125e5ace00c
sarahrami <118311187+sarahrami@users.noreply.github.com>
2023-02-19 19:07:24 -0500

    additem page updated

--- WebApp1/Models/Product.cs
+++ WebApp1/Models/Product.cs
@@ -6,6 +6,8 @@
         public string Name { get; set; }
         public string Description { get; set; }
         public string Cost { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
         public string Asset_condition { get; set; }
         public string Category { get; set; }
         public string Image { get; set; }
