e69aa171538d7b70d98dff1ad0246d3e79c56188
maverickPrakash <Prakashtheryon@gmail.com>
2023-02-19 20:48:01 -0500

    Databse

--- WebApp1/Controllers/HomeController.cs
+++ WebApp1/Controllers/HomeController.cs
@@ -6,12 +6,7 @@ namespace WebApp1.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+        
 
         public IActionResult Index()
         {
@@ -23,10 +18,7 @@ namespace WebApp1.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        
+        
     }
 }
---
+++ WebApp1/Models/BidSiteContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApp1.Models
+{
+    public class BidSiteContext: DbContext
+    {
+        public BidSiteContext(DbContextOptions<BidSiteContext> options)
+            : base(options) { }
+        public DbSet<Product> Products { get; set; }
+        public D
+    }
+}
