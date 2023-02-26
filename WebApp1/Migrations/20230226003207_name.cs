d99a503abae3659c200e4b5851bee54073f395a9
sarahrami <118311187+sarahrami@users.noreply.github.com>
2023-02-25 21:38:53 -0500

    edit page done and delete page

---
+++ WebApp1/Migrations/20230226003207_name.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApp1.Migrations
+{
+    public partial class name : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_AspNetUsers_Username",
+                table: "Products");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Username",
+                table: "Products",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_AspNetUsers_Username",
+                table: "Products",
+                column: "Username",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_AspNetUsers_Username",
+                table: "Products");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Username",
+                table: "Products",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_AspNetUsers_Username",
+                table: "Products",
+                column: "Username",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
